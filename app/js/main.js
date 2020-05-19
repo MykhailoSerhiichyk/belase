a3fb2cd6c33cf68a6a10a722aed3f00a768fd6b3
StreetDream <mishkasaltovskiy@gmail.com>
2020-05-19 17:50:48 +0300

    added section questions

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 $(function(){
 
-    
+    $('.questions__list li').on('click', function(){
+        $(this).toggleClass('active');
+        $(this).siblings().removeClass('active');
+    });
 
 });
 
